c57ab65fcc1e6b7658e43d06233c02a4076e107f
Rebekah Bertrand <134557739+RebekahAndrews928@users.noreply.github.com>
2024-12-05 12:58:41 -0500

    Update extension.ts to include add and remove library commands

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -4,7 +4,11 @@
  * ------------------------------------------------------------------------------------------ */
 
 import * as path from 'path';
-import { workspace, ExtensionContext } from 'vscode';
+import * as vscode from 'vscode';
+import * as fs from 'fs';
+import { LibraryView, Library } from './libraryView';
+
+
 
 import {
 	LanguageClient,
@@ -13,12 +17,22 @@ import {
 	TransportKind
 } from 'vscode-languageclient/node';
 
-let client: LanguageClient;
+let client: LanguageClient; 
+let libraryView: LibraryView;
 
-export function activate(context: ExtensionContext) {
+// Path to the libraries.json file
+const librariesFilePath = path.join(
+	vscode.workspace.workspaceFolders?.[0]?.uri.fsPath || '',
+	'libraries.json'
+);
+
+
+export function activate(context: vscode.ExtensionContext) {
+	
 	// The server is implemented in node
 	const serverModule = context.asAbsolutePath(
 		path.join('parasail-ls', 'out', 'server.js')
+
 	);
 
 	// If the extension is launched in debug mode then the debug server options are used
@@ -37,7 +51,7 @@ export function activate(context: ExtensionContext) {
 		documentSelector: [{ scheme: 'file', language: 'parasail' }],
 		synchronize: {
 			// Notify the server about file changes to '.clientrc files contained in the workspace
-			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
+			fileEvents: vscode.workspace.createFileSystemWatcher('**/.clientrc')
 		}
 	};
 
@@ -51,8 +65,60 @@ export function activate(context: ExtensionContext) {
 
 	// Start the client. This will also launch the server
 	client.start();
-}
 
+
+	libraryView = new LibraryView(librariesFilePath);
+	vscode.window.registerTreeDataProvider('libraryView', libraryView);
+
+
+    // Register command to add a library
+    const addLibraryCommand = vscode.commands.registerCommand('parasail.addLibrary', async () => {
+        const libraryPath = await vscode.window.showInputBox({
+            prompt: 'Enter the library path',
+            placeHolder: '/path/to/library',
+        });
+
+        if (libraryPath) {
+			// check if path exists
+			if (fs.existsSync(libraryPath)) {
+				const libraryName = path.basename(libraryPath);
+                libraryView.addLibraryPath({ name: libraryName, path: libraryPath });
+				vscode.window.showInformationMessage(`Library '${libraryName}' added.`);
+			} else{
+				vscode.window.showErrorMessage(`The path ${libraryPath} does not exist.`);
+			}
+		}
+    });
+
+    // Register command to remove a library
+    const removeLibraryCommand = vscode.commands.registerCommand('parasail.removeLibrary', async (library: Library) => {
+		if (library) {
+			// if library selected from the tree view
+			libraryView.removeLibraryPath(library);
+			vscode.window.showInformationMessage(`Library '${library.name}' removed.`);
+		} else {
+			// if no library selected, ask the user to enter a path
+			const libraryPath = await vscode.window.showInputBox({
+				prompt: 'Enter the path of the library to remove',
+				placeHolder: '/path/to/library',
+			});
+			if (libraryPath) {
+				const matchedLibrary = (await libraryView
+					.getChildren())
+					.find(lib => lib.path === libraryPath);
+				if (matchedLibrary) {
+					libraryView.removeLibraryPath(matchedLibrary);
+					vscode.window.showInformationMessage(`Library '${matchedLibrary.name}' removed.`);
+				} else {
+					vscode.window.showErrorMessage(`No library found with the path: ${libraryPath}`);
+				}
+		}
+		}
+	});
+
+	// Add to context subscriptions
+	context.subscriptions.push(addLibraryCommand, removeLibraryCommand);
+}
 export function deactivate(): Thenable<void> | undefined {
 	if (!client) {
 		return undefined;
